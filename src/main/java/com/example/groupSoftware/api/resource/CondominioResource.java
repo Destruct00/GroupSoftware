986c927199b7ce45fe0e8562c813b236e93ae895
Sandro <sandro.adriano001@gmail.com>
2021-05-08 10:19:36 -0300

    Inserido busca por ID em CondominioResource

--- src/main/java/com/example/groupSoftware/api/resource/CondominioResource.java
+++ src/main/java/com/example/groupSoftware/api/resource/CondominioResource.java
@@ -1,10 +1,13 @@
 package com.example.groupSoftware.api.resource;
 
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +19,18 @@ import com.example.groupSoftware.api.repository.CondominioRepository;
 @RestController
 @RequestMapping("/condominio")
 public class CondominioResource {
-
 	@Autowired
 	private CondominioRepository condominioRepository;
 	
-	
+	@GetMapping
 	public List<Condominio> listarCondominios(){
 		return condominioRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarProdutoId(@PathVariable String id){
+		Optional<Condominio> condominio = condominioRepository.findById(id);
+		return condominio.isPresent() ? ResponseEntity.ok(condominio) : ResponseEntity.notFound().build() ;
 	}
 	
 	@PostMapping()
